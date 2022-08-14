996f641af8e2fbfe6f6be4dc9b36d7cff9a1dfed
halomanlodestar <kunalranarj2005@gmail.com>
2022-08-14 17:45:52 +0530

    Completely revamped the page

--- src/main/index.js
+++ src/main/index.js
@@ -6,10 +6,12 @@ let LoggedIn = false;
 document.getElementById(`switch`).onclick = () => {
     if (Registered == true) {
         Registered = false;
+        document.getElementById(`switch`).innerText = `SignUp`;
         console.log(`is Registered : ${Registered}`);
     }
     else if (Registered == false) {
         Registered = true;
+        document.getElementById(`switch`).innerText = `Login`
         console.log(`is Registered : ${Registered}`);
     }
 };
