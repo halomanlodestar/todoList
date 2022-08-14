eba8dd433c65786f18fb3d6109db9417eebebc7c
halomanlodestar <kunalranarj2005@gmail.com>
2022-08-14 21:47:17 +0530

    v1.5 14-08-2022

--- src/main/index.js
+++ src/main/index.js
@@ -7,10 +7,12 @@ document.getElementById(`switch`).onclick = () => {
     if (Registered == true) {
         Registered = false;
         document.getElementById(`switch`).innerText = `SignUp`;
+        document.getElementById(`alterLink`).innerText = `Don't have an account?`
         console.log(`is Registered : ${Registered}`);
     }
     else if (Registered == false) {
         Registered = true;
+        document.getElementById(`alterLink`).innerText = `Already have account?`;
         document.getElementById(`switch`).innerText = `Login`
         console.log(`is Registered : ${Registered}`);
     }
